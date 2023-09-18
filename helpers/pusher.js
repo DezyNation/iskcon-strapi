252e47a539c84a5d83d91c91d965cba13ff6d847
DezyNation <dezynationindia@gmail.com>
2023-09-19 02:29:12 +0530

    port hidden

--- helpers/pusher.js
+++ helpers/pusher.js
@@ -2,12 +2,12 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
+  host: process.env.PUSHER_BASE_URL,
+  // port: process.env.PUSHER_PORT,
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_SECRET_KEY,
   useTLS: true,
-  host: process.env.PUSHER_BASE_URL,
-  port: process.env.PUSHER_PORT,
 });
 
 module.exports = pusher;
