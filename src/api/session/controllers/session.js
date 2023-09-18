e5793584d550a325b1edc3a90fa5792b9cba1d48
DezyNation <dezynationindia@gmail.com>
2023-09-19 02:07:58 +0530

    pusher

--- src/api/session/controllers/session.js
+++ src/api/session/controllers/session.js
@@ -1,16 +1,6 @@
 "use strict";
 
-const Pusher = require("pusher");
-
-// const { default: pusher } = require("../../../../helpers/pusher");
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_SECRET_KEY,
-  useTLS: true,
-  host: process.env.PUSHER_BASE_URL,
-  port: "443",
-});
+const { default: pusher } = require("../../../../helpers/pusher");
 
 const now = new Date();
 
