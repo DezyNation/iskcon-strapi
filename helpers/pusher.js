d5b0de0b33dcb23e972ef1e22cbd797eaefe8bfb
DezyNation <dezynationindia@gmail.com>
2023-09-19 02:41:43 +0530

    port added

--- helpers/pusher.js
+++ helpers/pusher.js
@@ -3,7 +3,7 @@ const Pusher = require("pusher");
 
 const pusher = new Pusher({
   host: process.env.PUSHER_BASE_URL,
-  // port: process.env.PUSHER_PORT,
+  port: process.env.PUSHER_PORT,
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_SECRET_KEY,
