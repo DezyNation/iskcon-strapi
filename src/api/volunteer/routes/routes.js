80ffea225c57c653f50a408f822a635181dfcb3e
DezyNation <dezynationindia@gmail.com>
2023-09-30 23:01:34 +0530

    get voters list

--- src/api/volunteer/routes/routes.js
+++ src/api/volunteer/routes/routes.js
@@ -1,7 +1,7 @@
 module.exports={
     routes: [
         {
-            method: 'POST',
+            method: 'GET',
             path: '/volunteers/view/all',
             handler: 'volunteer.fetchVolunteers',
             config: {
