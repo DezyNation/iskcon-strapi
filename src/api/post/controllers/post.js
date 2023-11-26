5781dcc31e2576db9c812b5a126b8a2e72f3b269
DezyNation <dezynationindia@gmail.com>
2023-11-27 02:09:12 +0530

    post reactions added

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -40,6 +40,14 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
               },
             },
           },
+          reactions: {
+            fields: ["id", "name", "username"],
+            populate: {
+              avatar: {
+                fields: ["url"],
+              },
+            },
+          },
         },
         sort: {
           createdAt: "desc",
@@ -50,6 +58,60 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
       console.log("Error while creating new post ");
       console.log(error);
       ctx.response.body = error;
+    }
+  },
+  likePost: async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const { user } = ctx.state;
+
+      if (!user) {
+        ctx.unauthorized("Please login to like this post");
+        return;
+      }
+
+      const like = await strapi.entityService.update(
+        "api::post.post",
+        parseInt(id),
+        {
+          data: {
+            reactions: {
+              connect: [user?.id],
+            },
+          },
+        }
+      );
+
+      ctx.body = like;
+    } catch (error) {
+      ctx.internalServerError = error;
+    }
+  },
+  unlikePost: async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const { user } = ctx.state;
+
+      if (!user) {
+        ctx.unauthorized("Please login to like this post");
+        return;
+      }
+
+      const like = await strapi.entityService.update(
+        "api::post.post",
+        parseInt(id),
+        {
+          data: {
+            reactions: {
+              connect: [user?.id],
+            },
+          },
+        }
+      );
+
+      ctx.body = like;
+    } catch (error) {
+      ctx.internalServerError = error;
     }
   },
 }));
--- src/api/post/routes/routes.js
+++ src/api/post/routes/routes.js
@@ -1,22 +1,40 @@
 module.exports = {
-    routes: [
-        {
-            method: 'POST',
-            path: '/posts/create',
-            handler: 'post.createNewPost',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        },
-        {
-            method: 'GET',
-            path: '/posts/view/all',
-            handler: 'post.getAllPosts',
-            config: {
-                policies: [],
-                middlewares: [],
-            },
-        }
-    ]
-}
+  routes: [
+    {
+      method: "POST",
+      path: "/posts/create",
+      handler: "post.createNewPost",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/posts/view/all",
+      handler: "post.getAllPosts",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/post/like/:id",
+      handler: "post.likePost",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/post/unlike/:id",
+      handler: "post.unlikePost",
+      config: {
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
